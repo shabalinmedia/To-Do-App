4d3fcbdf352fbdb0db87e5f7a3d7c55d00cacfa0
Alex Shabalin <shabalin.media@gmail.com>
2022-09-12 23:13:59 +0600

    Add Add Todo Button

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Buy milk", "Go for a walk", "Read a book"]
+    var itemArray = ["Buy milk", "Go for a walk", "Read a book"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,9 +40,37 @@ class TodoListViewController: UITableViewController {
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        var textField = UITextField()
         
+        let alert = UIAlertController(title: "Add New Todo Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+    
+        present(alert, animated: true, completion: nil)
         
     }
+    
+    
 }
 
