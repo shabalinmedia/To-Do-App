fe09af26cfb097d1b1fb5996c992818f7a204cb9
Alex Shabalin <shabalin.media@gmail.com>
2022-09-12 22:35:04 +0600

    Add Select and Unselect row

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -13,7 +13,6 @@ class TodoListViewController: UITableViewController {
     
     //MARK - Tableview Datasource Method
     
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -26,6 +25,24 @@ class TodoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         
         return cell
+    }
+    
+    //MARK - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
     }
 }
 
