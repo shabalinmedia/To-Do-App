69dd795c6fafb2289bfae5a9984d3f673f6c8ac1
Alex Shabalin <shabalin.media@gmail.com>
2022-09-14 23:28:17 +0600

    Add additional search bar methods

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -125,4 +125,16 @@ extension TodoListViewController: UISearchBarDelegate {
         
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+            
+        
+    }
 }
