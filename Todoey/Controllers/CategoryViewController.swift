37aa7fd6d6b0f5441e63071334ff990ca25697ab
Alex Shabalin <shabalin.media@gmail.com>
2022-09-15 10:44:28 +0600

    Add Delegate Methods

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -31,6 +31,21 @@ class CategoryViewController: UITableViewController {
         return cell
     }
     
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+    
+    
     
     //MARK: - Data Manipulation Methods
     
@@ -89,7 +104,7 @@ class CategoryViewController: UITableViewController {
     }
     
     
-    //MARK: - TableView Delegate Methods
+
     
     
     
